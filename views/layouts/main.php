a0252ebdc7596072e6ef8db2f46113e663218227
HWLin <hwlin1414@gmail.com>
2016-06-18 00:49:06 +0800

    remove contact

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,6 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
